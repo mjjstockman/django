b947abace80e4e2078bb886092492b135cd0f2db
mjjstockman <mjjstockman@gmail.com>
2022-01-20 09:28:54 +0000

    Migrate database and change attribute naming

--- todo/models.py
+++ todo/models.py
@@ -4,5 +4,9 @@ from django.db import models
 # inherit django's base model class
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.BooleanField(null=False, blank=False), default=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
     # django creates the id PK field
+
+    # override django's built in attribute naming convention 
+    def __str__(self):
+        return self.name
