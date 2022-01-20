68578d1d2fe88640364484fe6d4c2661e2b40e35
mjjstockman <mjjstockman@gmail.com>
2022-01-20 09:13:46 +0000

    Add Django model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# inherit django's base model class
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False), default=False)
+    # django creates the id PK field
